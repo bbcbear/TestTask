4f08a0d63f52668622b6ab09afb27c1c25d160f4
bbcbear <bbcbear@yandex.ru>
2019-05-03 13:14:22 +0300

    cleaned the code. corrected indents.

--- app/helpers/push.js
+++ app/helpers/push.js
@@ -6,41 +6,40 @@ module.exports = class Push{
  * @param {Object} options
  */
 
-constructor(queue, redisClient) {
- /*!
- * Module dependencies.
- */
-  const { Writable } = require('stream');
-  const { inherits } = require('util');
+  constructor(queue, redisClient) {
+  /*!
+  * Module dependencies.
+  */
+    const { Writable } = require('stream');
+    const { inherits } = require('util');
   
-/*!
- * Inherits from Writable.
- */
-  inherits(Push, Writable);
+  /*!
+  * Inherits from Writable.
+  */
+    inherits(Push, Writable);
 
-  Writable.call(this, { "objectMode": true });
+    Writable.call(this, { "objectMode": true });
 
-  this.queue = queue; // магия с очередью
-  this.redis = redisClient; // Обращение к редис
+    this.queue = queue;
+    this.redis = redisClient;
 
-  this.redis.on("error", function (err) { //обработка ошибок потока записи
-    this.end("error", err) //пишет в конец потока ошибку
-  });
+    this.redis.on("error", function (err) {
+      this.end("error", err)
+    });
+  }
 
-}
+  _write(data, e, next) {
+    const self = this;
 
-_write(data, e, next) {
-  const self = this;
+    self.redis.rpush(this.queue, JSON.stringify(data), function (err) {
+      if (err) return next(err);
+      self.emit('pushed', data);
+      next();
+    });
+  };
 
-  self.redis.rpush(this.queue, JSON.stringify(data), function (err) {
-    if (err) return next(err);
-    self.emit('pushed', data);
-    next();
-  });
-};
-
-end() { // предопределение поведение конца потока
-  Push.super_.prototype.end.call(this);
-  this.redis.end(false);
-};
+  end() {
+    Push.super_.prototype.end.call(this);
+    this.redis.end(false);
+  };
 }
