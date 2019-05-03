8c90ed16afe9b06e47f2607a27740914138d39db
bakugod <47696795+bakugod@users.noreply.github.com>
2019-05-03 18:32:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,55 @@
-const minimist = require('minimist');
-
 const Pull = require('./app/helpers/pull');
 const Push = require('./app/helpers/push');
 const notifier = require('./app/helpers/notifier');
 const redisClient = require('./app/helpers/redis');
 const Message = require('./app/models/Message');
 
+const minimist = require('minimist');
 /*
  * Get errors.
  */
-
 const argv = minimist(process.argv.slice(2));
-if (argv._.includes('getErrors'))
-  return createGetErrors();
+if (argv._.includes('getErrors')) return getErrors();
 
-/*!
- * Start worker.
+const TIMEOUT_DEFAULT = 500;
+
+/**
+ * Create get errors function
+ * @Pull { queue, redisClient, onTimeout, timeout }
+ * Listen data and show it in conole
+ * @return { Pull }
  */
 
-let worker = createWorker();
+function getErrors() {
+  let errors = new Pull('errors', redisClient(), () => errors.end(), TIMEOUT_DEFAULT);
+  errors.on("data", data => console.log(data));
+  return errors;
+}
 
-/*!
- * Events.
- */
-
-notifier.subscribe('idleTimeout', function () {
-  worker.end();
-  if (worker instanceof Push) return worker = createWorker();
-  if (worker instanceof Pull) return worker = createGenerator();
-});
-
-/*!
- * Create worker.
- */
+let worker = createWorker(); //Start worker
 
 function createWorker() {
   let errors = new Push('errors', redisClient());
-  let worker = new Pull('messages', redisClient(), function onTimeout() {
-    notifier.publish('idleTimeout', true);
-  });
+  let worker = new Pull('messages', redisClient(), () => {
+    Notifier.publish('idleTimeout', true);
+  }, TIMEOUT_DEFAULT);
 
-  worker.on("data", function (data) {
-    eventHandler(data, function (err, data) {
-      if (err) {
+  worker.on("data", (data) => {
+    eventHandler(data, (error, data) => {
+      if (error) {
         console.log('Error message', data);
         return errors.write(data);
       }
-      console.log('Read message', data);
+      console.log('Normal message', data);
     })
   });
 
   return worker;
 }
 
-function eventHandler(msg, callback) {
-  function onComplete() {
-    let error = Math.random() > 0.85;
-    callback(error, msg);
-  }
-
-  setTimeout(onComplete, Math.floor(Math.random() * 1000));
-}
-/*!
- * Create generator.
+/**
+ * Create generator, which push messages in Redis
+ * 
  */
 
 function createGenerator() {
@@ -72,10 +59,10 @@ function createGenerator() {
   (function next() {
     let message = Message.getMessage();
     generator.write(message);
-    console.log('Write message', message);
-    client.llen('messages', (err, length) => {
-      if (length > 100000){
-        return notifier.publish('idleTimeout', true);
+    //console.log(' ', message);
+    client.llen('messages', ( _, length) => {
+      if (length > 0){
+        return Notifier.publish('idleTimeout', true);
       }  
       next();
     });
@@ -83,12 +70,31 @@ function createGenerator() {
   return generator;
 }
 
-/*!
- * Create get errors.
+/**
+ * Create state-machine function,
+ * which choose what worker we create
+ * 
+ * @return { Function }
  */
 
-function createGetErrors() {
-  let errors = new Pull('errors', redisClient(), () => errors.end(), 0);
-  errors.on("data", (data) => console.log(data));
-  return errors;
+Notifier.subscribe('idleTimeout', function () {
+  worker.end();
+  if (worker instanceof Push) return worker = createWorker();
+  if (worker instanceof Pull) return worker = createGenerator();
+});
+
+/**
+ * Standart function from original text
+ * C&P programming style
+ * 
+ * @param { String } msg 
+ * @param { Function } callback 
+ */
+
+function eventHandler(msg, callback) {
+  function onComplete() {
+    let error = Math.random() > 0.95;
+    callback(error, msg);
+  }
+  setTimeout(onComplete, 1000);
 }
