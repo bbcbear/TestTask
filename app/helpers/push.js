04d0c01d2d60b36a88bb090a660fcc71c5076315
bbcbear <bbcbear@yandex.ru>
2019-05-03 12:02:21 +0300

    Push convert to class

--- app/helpers/push.js
+++ app/helpers/push.js
@@ -1,9 +1,4 @@
-/*!
- * Module dependencies.
- */
-
-const Writable = require('stream').Writable;
-const inherits = require("util").inherits;
+module.exports = class Push{
 
 /**
  * Push constructor
@@ -11,21 +6,30 @@ const inherits = require("util").inherits;
  * @param {Object} options
  */
 
-function Push(queue, redisClient) { //можно переписать по es6
-  const self = this;
+constructor(queue, redisClient) {
+ /*!
+ * Module dependencies.
+ */
+  const { Writable } = require('stream');
+  const { inherits } = require('util');
+  
+/*!
+ * Inherits from Writable.
+ */
+  inherits(Push, Writable);
 
-  Writable.call(self, { "objectMode": true });
+  Writable.call(this, { "objectMode": true });
 
-  self.queue = queue; // магия с очередью
-  self.redis = redisClient; // Обращение к редис
+  this.queue = queue; // магия с очередью
+  this.redis = redisClient; // Обращение к редис
 
-  self.redis.on("error", function (err) { //обработка ошибок потока записи
-    self.end("error", err) //пишет в конец потока ошибку
+  this.redis.on("error", function (err) { //обработка ошибок потока записи
+    this.end("error", err) //пишет в конец потока ошибку
   });
 
 }
 
-Push.prototype._write = function (data, e, next) {
+_write(data, e, next) {
   const self = this;
 
   self.redis.rpush(this.queue, JSON.stringify(data), function (err) {
@@ -35,19 +39,8 @@ Push.prototype._write = function (data, e, next) {
   });
 };
 
-Push.prototype.end = function () { // предопределение поведение конца потока
+end() { // предопределение поведение конца потока
   Push.super_.prototype.end.call(this);
   this.redis.end(false);
 };
-
-/*!
- * Inherits from Writable.
- */
-
-inherits(Push, Writable); //наследуем в пуш врайтбл
-
-/*!
- * Expose Push.
- */
-
-module.exports = Push; //какой то наркоманский экспорт вырезал "module.exports = exports = Push;"
+}
